2f8542ca4d43606562d92d534e5a56acd638f384
Daniel Cashman <dan.a.cashman@gmail.com>
2017-01-30 13:29:47 -0800

    Add chapter 0 exercises.

---
+++ ch00/0.cpp
@@ -0,0 +1,9 @@
+// compile and run hello world program (copied below)
+// run g++ on this file.
+#include <iostream>
+
+int main()
+{
+  std::cout << "Hello, world!" << std::endl;
+  return 0;
+}
---
+++ ch00/4.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+  /* print out hello world pgm using chpt 0 concepts.
+     spaces not tabs! */
+    std::cout << "#include <iostream>"  << std::endl << std::endl
+    << "int main()" << std::endl << "{" << std::endl
+    << "    std::cout << \"Hello, world!\" << std::endl;" << std::endl
+    << "    return 0;" << std::endl << "}" << std::endl;
+    return 0;
+}
+    
